25c3dab92d48457c0057f1347cda2b39107cb72c
Gabrielle KAUFMANN <gabrielle.kaufmann@meteo.fr>
2018-06-14 15:47:04 +0200

    Read README.md in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
 setup(
     name='excel2wisxml',
     version='3.5.4-1',
