2bda3a5af28f0b37bc4f3a47fe53c19db4e558b0
Jean-Marie Damas <jean-marie.damas@meteo.fr>
2019-04-11 16:24:34 +0200

    Forced python2 build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# Uninstall excel2wisxml python package
 pip2 uninstall excel2wisxml
 rm dist/*
-<<<<<<< HEAD
+# Install setuptools
+pip2 install setuptools
+# Create excel2wisxml python package in dist directory
 python2 setup.py sdist
+# Install excel2wisxml package
 pip2 install dist/excel2wisxml-*.tar.gz --user
-=======
-# Create python package in dist directory
-pip install setuptools
-python setup.py sdist
-pip install dist/excel2wisxml-*.tar.gz --user
->>>>>>> c3a4af4dd98696d0dd6aa2f10a6aaed2947fe0f2
